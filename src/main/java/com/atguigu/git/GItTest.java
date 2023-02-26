1ad9ab034bbe308aba2539aa0786352d820a9e23
Lanlijun888 <3143826150@qq.com>
2023-02-27 00:12:27 +0800

    third commit

--- src/main/java/com/atguigu/git/GItTest.java
+++ src/main/java/com/atguigu/git/GItTest.java
@@ -4,6 +4,7 @@ public class GItTest {
     public static void main(String[] args){
         System.out.println("Hello git!");
         System.out.println("Hello git2!");
+        System.out.println("Hello git3!");
     }
 
 }
